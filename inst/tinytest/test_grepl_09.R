d0d6441b1ae962c7fee28a5df51c10cbba917ff7
HughParsonage <hugh.parsonage@gmail.com>
2021-08-04 16:09:40 +1000

    Test interrupted number

--- inst/tinytest/test_grepl_09.R
+++ inst/tinytest/test_grepl_09.R
@@ -3,6 +3,7 @@ s <- c("AB", "abc ", "", NA, "90", "ABC090")
 expect_equal(dauphin:::grepl_digit(s), grepl("[0-9]", s))
 expect_equal(dauphin:::gsub_09("1 234"), 1234)
 expect_equal(dauphin:::gsub_09(c("000 2342", NA)), c(2342L, NA))
+expect_equal(dauphin:::gsub_09(c("000 2342", c("123 abc 234"))), c(123L, NA))
 
 
 
